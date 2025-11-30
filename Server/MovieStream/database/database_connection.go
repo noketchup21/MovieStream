06653829763f14f000cf0a3178c7214e33a34753
noketchup21 <luongnguyenduykhang2782005@gmail.com>
2025-11-30 13:02:00 +0700

    configure database connection

---
+++ Server/MovieStream/database/database_connection.go
@@ -0,0 +1,56 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func Connect() *mongo.Client {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Println("Unable to find .env file")
+	}
+
+	MongoDb := os.Getenv("MONGODB_URI")
+
+	if MongoDb == "" {
+		log.Fatal("MONGODB_URI not found in environment variables")
+	}
+
+	fmt.Println("Connecting to MongoDB at", MongoDb)
+
+	clientOptions := options.Client().ApplyURI(MongoDb)
+
+	client, err := mongo.Connect(nil, clientOptions)
+
+	if err != nil {
+		return nil
+	}
+	return client
+}
+
+var Client *mongo.Client = Connect()
+
+func OpenCollection(collectionName string) *mongo.Collection {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Println("Unable to find .env file")
+	}
+
+	databaseName := os.Getenv("DATABASE_NAME")
+	if databaseName == "" {
+		log.Fatal("DATABASE_NAME not found in environment variables")
+	}
+	collection := Client.Database(databaseName).Collection(collectionName)
+	if collection == nil {
+		log.Fatalf("Collection %s not found in database %s", collectionName, databaseName)
+	}
+	return collection
+}
