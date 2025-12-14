c23090f55f3ebb4172ca571e006f31864293e33a
noketchup21 <luongnguyenduykhang2782005@gmail.com>
2025-12-14 11:11:00 +0700

    add createmovie func and swagger

--- Server/MovieStream/controllers/movie_controller.go
+++ Server/MovieStream/controllers/movie_controller.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/noketchup21/MovieStream/Server/MovieStream/database"
 	model "github.com/noketchup21/MovieStream/Server/MovieStream/models"
 	"go.mongodb.org/mongo-driver/v2/bson"
@@ -13,52 +14,94 @@ import (
 )
 
 var movieCollection *mongo.Collection = database.OpenCollection("movies")
+var validate = validator.New()
 
-func GetMovies() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
-		defer cancel()
+// GetMovies godoc
+// @Summary Get all movies
+// @Tags Movies
+// @Produce json
+// @Success 200 {array} model.Movie
+// @Router /movies [get]
+func GetMovies(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
 
-		var movies []model.Movie
+	var movies []model.Movie
 
-		cursor, err := movieCollection.Find(ctx, bson.M{})
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch movies"})
-		}
-		defer cursor.Close(ctx)
-
-		if err = cursor.All(ctx, &movies); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode movies"})
-		}
-		c.JSON(http.StatusOK, movies)
+	cursor, err := movieCollection.Find(ctx, bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch movies"})
+		return
 	}
+	defer cursor.Close(ctx)
+
+	if err = cursor.All(ctx, &movies); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode movies"})
+		return
+	}
+
+	c.JSON(http.StatusOK, movies)
 }
 
-func GetMovie() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
-		defer cancel()
+// GetMovie godoc
+// @Summary Get movie by IMDb ID
+// @Tags Movies
+// @Param imdb_id path string true "IMDb ID"
+// @Produce json
+// @Success 200 {object} model.Movie
+// @Failure 404 {object} map[string]string
+// @Router /movies/{imdb_id} [get]
+func GetMovie(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
 
-		movieId := c.Param("imdb_id")
-		if movieId == "" {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Movie ID is required"})
-			return
-		}
-
-		var movie model.Movie
-		err := movieCollection.FindOne(ctx, bson.M{"imdb_id": movieId}).Decode(&movie)
-		if err != nil {
-			c.JSON(http.StatusNotFound, gin.H{"error": "Movie not found"})
-			return
-		}
-
-		c.JSON(http.StatusOK, movie)
+	movieId := c.Param("imdb_id")
+	if movieId == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Movie ID is required"})
+		return
 	}
+
+	var movie model.Movie
+	err := movieCollection.FindOne(ctx, bson.M{"imdb_id": movieId}).Decode(&movie)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Movie not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, movie)
 }
 
-func CreateMovie() gin.HandlerFunc {
-	return func(c *gin.Context) {
+// CreateMovie godoc
+// @Summary Create a movie
+// @Tags Movies
+// @Accept json
+// @Produce json
+// @Param movie body model.Movie true "Movie"
+// @Success 201 {object} map[string]interface{}
+// @Router /createmovie [post]
+func CreateMovie(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
 
+	var movie model.Movie
+	if err := c.ShouldBindJSON(&movie); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
 	}
+
+	if err := validate.Struct(movie); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Validation failed",
+			"details": err.Error(),
+		})
+		return
+	}
+
+	result, err := movieCollection.InsertOne(ctx, movie)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create movie"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
--- Server/MovieStream/main.go
+++ Server/MovieStream/main.go
@@ -5,16 +5,27 @@ import (
 
 	"github.com/gin-gonic/gin"
 	controller "github.com/noketchup21/MovieStream/Server/MovieStream/controllers"
+	_ "github.com/noketchup21/MovieStream/Server/MovieStream/docs"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title MovieStream API
+// @version 1.0
+// @description Movie API using Gin
+// @host localhost:8080
+// @BasePath /
 func main() {
 	router := gin.Default()
+	// Swagger endpoint
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.GET("/hello", func(c *gin.Context) {
 		c.String(200, "Hello, World!")
 	})
 
-	router.GET("/movies", controller.GetMovies())
-	router.GET("/movies/:imdb_id", controller.GetMovie())
+	router.GET("/movies", controller.GetMovies)
+	router.GET("/movies/:imdb_id", controller.GetMovie)
+	router.POST("/createmovie", controller.CreateMovie)
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server:", err)
