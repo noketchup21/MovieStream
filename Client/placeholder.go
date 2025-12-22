f8a1d879b2d5240830599667264f9e8e2f138478
noketchup21 <luongnguyenduykhang2782005@gmail.com>
2025-12-22 21:43:52 +0700

    Update placeholder.go
    Fix gitignore for env and exe files
    
    fix gitignore
    
    Stop tracking Server/MovieStream folder
    
    fix stuff

--- Client/placeholder.go
+++ Client/placeholder.go
@@ -1,3 +1,5 @@
 package placeholder
 
 // This is a placeholder file to ensure the 'Client' directory is included in version control.
+//testing
+// Actual client implementation will be added here in the future.
